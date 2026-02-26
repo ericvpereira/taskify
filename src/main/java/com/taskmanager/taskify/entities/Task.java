496d904b9279265d6a19de2135b32d98183fae56
Eric Vieira <ericvieira.p@outlook.com>
2026-02-25 22:17:42 -0300

    H2 database, test profile, JPA

--- src/main/java/com/taskmanager/taskify/entities/Task.java
+++ src/main/java/com/taskmanager/taskify/entities/Task.java
@@ -6,10 +6,20 @@ import java.util.Objects;
 
 import com.taskmanager.taskify.entities.enums.TaskStatus;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_task")
 public class Task implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
 	private String description;
